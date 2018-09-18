74cd8c2f5e8caaa068477f031058a7e32bcaf894
Anis Bouhadida <AB.geekanis@outlook.fr>
2018-09-18 11:35:13 +0200

    Minify CSS ans JS files

---
+++ script-min.js
@@ -0,0 +1,7 @@
+let container=document.querySelector(".container");function createGrid(width){for(let row=0;row<width;row++){const gridRow=document.createElement("div");gridRow.classList.add("gridRow");gridRow.style.height=(100/width)+"%";for(let colomn=0;colomn<width;colomn++){const grid=document.createElement("div");grid.classList.add("grid");grid.style.width=(100/width)+"%";gridRow.appendChild(grid)}
+container.appendChild(gridRow)}
+changeColor()}
+function getRandomIntInclusive(min,max){min=Math.ceil(min);max=Math.floor(max);return Math.floor(Math.random()*(max-min+1))+min}
+function changeColor(){let cell=document.querySelectorAll(".grid");cell.forEach(cel=>{cel.addEventListener("mouseenter",(e)=>{e.target.style.backgroundColor="rgb("+getRandomIntInclusive(0,225)+","+getRandomIntInclusive(0,225)+","+getRandomIntInclusive(0,225)+")"})})}
+function reset(){let children=document.querySelectorAll(".gridRow");children.forEach(child=>{container.removeChild(child)})
+let getGridSize=prompt("give a size for the grid","16");createGrid(getGridSize)}
